5a7ae2adf6c3da6e77895f598e5e607bf9c2b84e
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-08-06 22:03:55 -0300

    feat: create /users and /group routes

--- help_or_gtfo_backend/urls.py
+++ help_or_gtfo_backend/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 import help_or_gtfo_backend.quickstart.views as views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('quickstart/', views.UserViewSet.as_view({ 'get': 'list' }))
+    path('', include(router.urls)),
 ]
