05a28fb929374bfcb4c8e1b5c7707dbf2a95f0b7
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-08-10 23:49:43 -0300

    chore: remove unused urls

--- help_or_gtfo_backend/urls.py
+++ help_or_gtfo_backend/urls.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
-import help_or_gtfo_backend.quickstart.views as views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
 ]
