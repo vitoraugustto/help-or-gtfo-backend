bd22e0fade9a92fa87679dc2e4fb48ae45af92ef
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-08-14 18:26:30 -0300

    feat: create Expeditions model

--- rundown/models.py
+++ rundown/models.py
@@ -12,3 +12,32 @@ class Rundown(models.Model):
 
     def __str__(self):
         return f'ALT://RUNDOWN {self.number}.0'
+
+class Expeditions(models.Model):
+    TIERS = [
+        ('A', 'A'),
+        ('B', 'B'),
+        ('C', 'C'),
+        ('D', 'D'),
+        ('E', 'E'),
+    ]
+
+    DIFFICULTIES = [
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('X', 'X')
+    ]
+
+    title = models.CharField(blank=False, null=False, max_length=20, verbose_name='Title')
+    tier = models.CharField(blank=False, null=False, max_length=1, choices=TIERS, verbose_name='Tiers')
+    difficulty = models.CharField(blank=False, null=False, max_length=1, choices=DIFFICULTIES, verbose_name='Difficulty') 
+    rundown = models.ForeignKey(Rundown, blank=False, null=False, on_delete=models.CASCADE, verbose_name='Rundown')
+
+    class Meta:
+        verbose_name = 'Expedition'
+        verbose_name_plural = 'Expeditions'
+
+    def __str__(self):
+        return f'R{self.rundown.number}{self.tier}{self.difficulty}'
