ce54faef807c6e5a4389e58fdbbd01fe64536398
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-08-13 23:47:08 -0300

    feat: add is_superuser to CustomUser model

--- user/models.py
+++ user/models.py
@@ -6,8 +6,9 @@ from .managers import CustomUserManager
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_("email address"), unique=True)
-    is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
 
     xp = models.PositiveIntegerField(null=False, blank=False, default=0, verbose_name='Experience')
