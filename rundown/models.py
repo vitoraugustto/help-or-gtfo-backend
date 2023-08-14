4c3d2c58b8117711066eb4d06dad3b6773ecdeeb
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-08-14 17:13:50 -0300

    feat: create Rundown model

--- rundown/models.py
+++ rundown/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Rundown(models.Model):
+    title = models.CharField(blank=False, null=False, max_length=20, verbose_name='Title')
+    number = models.PositiveIntegerField(blank=False, null=False, verbose_name='Number')
+    release_date = models.DateField(blank=False, null=False, verbose_name='Release date')
+
+    class Meta:
+        verbose_name = 'Rundown'
+        verbose_name_plural = 'Rundowns'
+        ordering = ['number']
+
+    def __str__(self):
+        return f'ALT://RUNDOWN {self.number}.0'
