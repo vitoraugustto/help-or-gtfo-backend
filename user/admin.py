c619b919e10f21cda1edc33059f5c4ed947a86c4
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-08-14 00:48:01 -0300

    feat: create CustomUserAdmin

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
-from .models import CustomUser
 from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-admin.site.register(CustomUser, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    list_display = ('email', 'xp', 'is_active', 'is_staff', 'is_superuser')
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
+    search_fields = ('email',)
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Custom Fields', {'fields': ('xp',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
