47804f8f4c5dffb83a65a602893d96efe81abd98
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-08-14 18:41:19 -0300

    feat: create Expedition admin

--- rundown/admin.py
+++ rundown/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Rundown
+from .models import Rundown, Expedition
+
+class ExpeditionInline(admin.TabularInline):
+    model = Expedition
+    extra = 0
 
 class RundownAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'title', 'release_date')
+    inlines = [ExpeditionInline]
+
+class ExpediditonAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'title', 'rundown')
 
 admin.site.register(Rundown, RundownAdmin)
+admin.site.register(Expedition, ExpediditonAdmin)
