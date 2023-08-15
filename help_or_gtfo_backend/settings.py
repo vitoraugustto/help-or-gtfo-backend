e4e1855d2c8fa5fd577cc63f50a7158bea0927cc
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-08-15 14:10:32 -0300

    feat: load dotenv in settings.py

--- help_or_gtfo_backend/settings.py
+++ help_or_gtfo_backend/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+# Load environment variables
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
