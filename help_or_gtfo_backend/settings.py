b4cc16941b0a31c5170c0ee5b565792a3747e2b9
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-08-15 13:21:30 -0300

    refactor: remove unnecessary variable PROJECT_DIR

--- help_or_gtfo_backend/settings.py
+++ help_or_gtfo_backend/settings.py
@@ -118,9 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-PROJECT_DIR = os.path.dirname(os.path.dirname(__file__) + "/../")
 STATIC_URL = "/static/"
-STATIC_ROOT = PROJECT_DIR + "/static"
+STATIC_ROOT = BASE_DIR + "/static"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
