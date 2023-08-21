836dc432268d77a431022b47b9b6557d1dc54042
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-08-21 01:13:29 -0300

    feat: create ExpeditionSerializer

--- rundown/serializers.py
+++ rundown/serializers.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from rest_framework import serializers
-from .models import Rundown
+from .models import Rundown, Expedition
+
 
 class RundownSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rundown
-        fields = ['id', 'title', 'number', 'release_date']
+        fields = ["id", "title", "number", "release_date"]
+
+
+class ExpeditionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = (Expedition,)
+        fields = ["id", "title", "tier", "difficulty", "xp"]
