d46f72b9700769b4f32dca71d3b5e30270826195
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-08-21 01:35:55 -0300

    feat: define ExpeditionSerializer before RundownSerializer

--- rundown/serializers.py
+++ rundown/serializers.py
@@ -3,13 +3,13 @@ from rest_framework import serializers
 from .models import Rundown, Expedition
 
 
+class ExpeditionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Expedition
+        fields = ["id", "title", "tier", "difficulty", "xp", "rundown"]
+
+
 class RundownSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rundown
         fields = ["id", "title", "number", "release_date"]
-
-
-class ExpeditionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = (Expedition,)
-        fields = ["id", "title", "tier", "difficulty", "xp"]
