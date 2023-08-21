de6a141762b54cfe8b43c1c8dac5a8880e8d4720
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-08-21 01:36:25 -0300

    style: lint rundown/models.py with black

--- rundown/models.py
+++ rundown/models.py
@@ -1,56 +1,66 @@
 from django.db import models
 
+
 class Rundown(models.Model):
-    title = models.CharField(blank=False, null=False, max_length=20, verbose_name='Title')
-    number = models.PositiveIntegerField(blank=False, null=False, verbose_name='Number')
-    release_date = models.DateField(blank=False, null=False, verbose_name='Release date')
+    title = models.CharField(
+        blank=False, null=False, max_length=20, verbose_name="Title"
+    )
+    number = models.PositiveIntegerField(blank=False, null=False, verbose_name="Number")
+    release_date = models.DateField(
+        blank=False, null=False, verbose_name="Release date"
+    )
 
     class Meta:
-        verbose_name = 'Rundown'
-        verbose_name_plural = 'Rundowns'
-        ordering = ['number']
+        verbose_name = "Rundown"
+        verbose_name_plural = "Rundowns"
+        ordering = ["number"]
 
     def __str__(self):
-        return f'ALT://RUNDOWN {self.number}.0'
+        return f"ALT://RUNDOWN {self.number}.0"
+
 
 class Expedition(models.Model):
     TIERS = [
-        ('A', 'A'),
-        ('B', 'B'),
-        ('C', 'C'),
-        ('D', 'D'),
-        ('E', 'E'),
+        ("A", "A"),
+        ("B", "B"),
+        ("C", "C"),
+        ("D", "D"),
+        ("E", "E"),
     ]
 
-    DIFFICULTIES = [
-        ('1', '1'),
-        ('2', '2'),
-        ('3', '3'),
-        ('4', '4'),
-        ('X', 'X')
-    ]
+    DIFFICULTIES = [("1", "1"), ("2", "2"), ("3", "3"), ("4", "4"), ("X", "X")]
 
-    title = models.CharField(blank=False, null=False, max_length=20, verbose_name='Title')
-    tier = models.CharField(blank=False, null=False, max_length=1, choices=TIERS, verbose_name='Tiers')
-    difficulty = models.CharField(blank=False, null=False, max_length=1, choices=DIFFICULTIES, verbose_name='Difficulty') 
-    rundown = models.ForeignKey(Rundown, blank=False, null=False, on_delete=models.CASCADE, verbose_name='Rundown')
-    xp = models.PositiveIntegerField(null=False, blank=False, verbose_name='Experience')
+    title = models.CharField(
+        blank=False, null=False, max_length=20, verbose_name="Title"
+    )
+    tier = models.CharField(
+        blank=False, null=False, max_length=1, choices=TIERS, verbose_name="Tiers"
+    )
+    difficulty = models.CharField(
+        blank=False,
+        null=False,
+        max_length=1,
+        choices=DIFFICULTIES,
+        verbose_name="Difficulty",
+    )
+    rundown = models.ForeignKey(
+        Rundown,
+        blank=False,
+        null=False,
+        on_delete=models.CASCADE,
+        verbose_name="Rundown",
+    )
+    xp = models.PositiveIntegerField(null=False, blank=False, verbose_name="Experience")
 
     class Meta:
-        verbose_name = 'Expedition'
-        verbose_name_plural = 'Expeditions'
+        verbose_name = "Expedition"
+        verbose_name_plural = "Expeditions"
 
     def __str__(self):
-        return f'R{self.rundown.number}{self.tier}{self.difficulty}'
+        return f"R{self.rundown.number}{self.tier}{self.difficulty}"
 
     def save(self, *args, **kwargs):
-        tier_xp_mapping = {
-            'A': 20,
-            'B': 30,
-            'C': 50,
-            'D': 75,
-            'E': 100
-        }
+        tier_xp_mapping = {"A": 20, "B": 30, "C": 50, "D": 75, "E": 100}
 
         if self.tier:
             self.xp = tier_xp_mapping.get(self.tier, 0)
