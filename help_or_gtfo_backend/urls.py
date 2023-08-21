1a5ba3af2129dbe6578c22fac86a86ecc1821e15
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-08-21 01:09:26 -0300

    feat: create RundownView

--- help_or_gtfo_backend/urls.py
+++ help_or_gtfo_backend/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from rundown.views import RundownView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/v1/rundowns", RundownView.as_view()),
 ]
--- rundown/views.py
+++ rundown/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import RundownSerializer
+from .models import Rundown
 
-# Create your views here.
+
+class RundownView(APIView):
+    def get(self, request):
+        queryset = Rundown.objects.all()
+        serializer = RundownSerializer(queryset, many=True)
+
+        return Response(serializer.data)
