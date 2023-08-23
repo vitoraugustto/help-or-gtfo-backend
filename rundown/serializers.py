0ed5d210c7cba09b9fabd0a1fc94908dc206d7d8
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-08-23 01:26:46 -0300

    feat: add expeditions in RundownSerializer

--- rundown/serializers.py
+++ rundown/serializers.py
@@ -10,6 +10,8 @@ class ExpeditionSerializer(serializers.ModelSerializer):
 
 
 class RundownSerializer(serializers.ModelSerializer):
+    expeditions = ExpeditionSerializer(many=True, source="expedition_set")
+
     class Meta:
         model = Rundown
-        fields = ["id", "title", "number", "release_date"]
+        fields = ["id", "title", "number", "release_date", "expeditions"]
