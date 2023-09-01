27a6a0e37421dbb5f6d52881856d66223b99e9b6
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-08-31 22:48:15 -0300

    feat: set date_joined and last_login as readonly_fields

--- user/admin.py
+++ user/admin.py
@@ -2,26 +2,35 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
+
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
 
-    list_display = ('email', 'xp', 'is_active', 'is_staff', 'is_superuser')
-    list_filter = ('is_active', 'is_staff', 'is_superuser')
-    search_fields = ('email',)
-    ordering = ('email',)
+    list_display = ("email", "xp", "is_active", "is_staff", "is_superuser")
+    list_filter = ("is_active", "is_staff", "is_superuser")
+    search_fields = ("email",)
+    ordering = ("email",)
+    readonly_fields = ("date_joined", "last_login")
 
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Custom Fields', {'fields': ('xp',)}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        (None, {"fields": ("email", "password")}),
+        ("Custom Fields", {"fields": ("xp",)}),
+        (
+            "Permissions",
+            {"fields": ("is_active", "is_staff", "is_superuser", "user_permissions")},
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
 
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2'),
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
     )
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
