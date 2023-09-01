8d8b3c39bea53840b65e73922b3d25abbf52249e
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-08-31 22:41:27 -0300

    feat: add CustomUserView in urls.py

--- help_or_gtfo_backend/urls.py
+++ help_or_gtfo_backend/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from rundown.views import RundownView
+from user.views import CustomUserView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/rundowns", RundownView.as_view()),
+    path("api/v1/prisoners", CustomUserView.as_view()),
 ]
