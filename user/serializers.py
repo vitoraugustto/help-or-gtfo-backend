49eee37e945bd42a26a4fdd8b6982542143dc598
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-09-14 23:04:02 -0300

    feat: add username in serializers.py

--- user/serializers.py
+++ user/serializers.py
@@ -6,4 +6,4 @@ from .models import CustomUser
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ["id", "email", "is_active", "is_staff", "is_superuser", "date_joined"]
+        fields = ["id", "email", "username", "is_active", "is_staff", "is_superuser", "date_joined"]
