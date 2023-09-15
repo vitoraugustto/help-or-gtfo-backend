4c1510a9570404744f92cede42ca4e8656f10bce
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-09-14 23:03:46 -0300

    feat: add username in command craete_superuser

--- user/management/commands/create_superuser.py
+++ user/management/commands/create_superuser.py
@@ -7,10 +7,11 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         email = os.getenv('DJANGO_SUPERUSER_EMAIL')
+        username = os.getenv('DJANGO_SUPERUSER_USERNAME')
         password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
 
         if not CustomUser.objects.filter(email=email).exists():
-            CustomUser.objects.create_superuser(email=email, password=password)
+            CustomUser.objects.create_superuser(email=email, username=username, password=password)
 
             self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
         else:
