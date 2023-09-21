5de7aba722d1078d64f7259e2a536bd9548540cb
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-09-21 00:48:53 -0300

    feat: create CompletedExpeditionsInline with completed_expeditions

--- user/admin.py
+++ user/admin.py
@@ -3,9 +3,20 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 
+class CompletedExpeditionsInline(admin.TabularInline):
+    verbose_name = "Completed expedition"
+    verbose_name_plural = "Completed expeditions"
+    model = CustomUser.completed_expeditions.through
+    extra = 0
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
 
+    inlines = [CompletedExpeditionsInline]
     list_display = ("username", "email", "xp", "is_active", "is_staff", "is_superuser")
     list_filter = ("is_active", "is_staff", "is_superuser")
     search_fields = ("email",)
@@ -13,7 +24,8 @@ class CustomUserAdmin(UserAdmin):
     readonly_fields = ("date_joined", "last_login")
 
     fieldsets = (
-        ("Basic information", {"fields": ("username", "email", "password", "xp")}),
+        ("Basic information", {"fields": ("username", "email", "password")}),
+        ("Complex", {"fields": ("xp", "completed_expeditions")}),
         ("Dates", {"fields": ("last_login", "date_joined")}),
         (
             "Permissions",
--- user/models.py
+++ user/models.py
@@ -2,12 +2,13 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+from rundown.models import Expedition
 from .managers import CustomUserManager
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_("email address"), unique=True)
-    username = models.CharField(max_length=20, unique=True) 
+    username = models.CharField(max_length=20, unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
@@ -15,6 +16,10 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     xp = models.PositiveIntegerField(
         null=False, blank=False, default=0, verbose_name="Experience"
+    )
+
+    completed_expeditions = models.ManyToManyField(
+        Expedition, default=None, verbose_name="Completed expeditions"
     )
 
     USERNAME_FIELD = "username"
