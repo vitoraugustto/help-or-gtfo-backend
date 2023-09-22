b40afe652e77e8949fa47530211bfdab4e712ea4
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-09-21 23:57:06 -0300

    feat: create MinifiedCustomUserSerializer

--- user/serializers.py
+++ user/serializers.py
@@ -6,4 +6,18 @@ from .models import CustomUser
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ["id", "email", "username", "is_active", "is_staff", "is_superuser", "date_joined"]
+        fields = [
+            "id",
+            "email",
+            "username",
+            "is_active",
+            "is_staff",
+            "is_superuser",
+            "date_joined",
+        ]
+
+
+class MinifiedCustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ["id", "email", "username"]
