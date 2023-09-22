45e18296c76be8fc1fdff8b0572b9e7fe0be3fc5
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-09-21 23:54:41 -0300

    feat: create MinifiedExpeditionSerializer

--- rundown/serializers.py
+++ rundown/serializers.py
@@ -1,16 +1,47 @@
 from django.shortcuts import render
 from rest_framework import serializers
 from .models import Rundown, Expedition
+from user.models import CustomUser
+from user.serializers import CustomUserSerializer
+
+
+class MinifiedExpeditionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Expedition
+        fields = [
+            "id",
+            "display_name",
+            "title",
+            "tier",
+            "difficulty",
+            "xp",
+        ]
 
 
 class ExpeditionSerializer(serializers.ModelSerializer):
+    finishers = serializers.SerializerMethodField()
+
     class Meta:
         model = Expedition
-        fields = ["id", "display_name", "title", "tier", "difficulty", "xp"]
+        fields = [
+            "id",
+            "display_name",
+            "title",
+            "tier",
+            "difficulty",
+            "xp",
+            "finishers",
+        ]
+
+    def get_finishers(self, obj):
+        finishers = CustomUser.objects.filter(completed_expeditions=obj)
+        serializer = CustomUserSerializer(finishers, many=True)
+
+        return serializer.data
 
 
 class RundownSerializer(serializers.ModelSerializer):
-    expeditions = ExpeditionSerializer(many=True, source="expedition_set")
+    expeditions = MinifiedExpeditionSerializer(many=True, source="expedition_set")
 
     class Meta:
         model = Rundown
