2ae7cf15726a10ce1e9f34383ae1300780ef6929
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-10-03 19:19:10 -0300

    feat: improve CustomUserView GET method

--- user/views.py
+++ user/views.py
@@ -2,13 +2,22 @@ from django.shortcuts import render
 from help_or_gtfo_backend.utils import success_response
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import CustomUserSerializer
+from .serializers import MinifiedCustomUserSerializer
 from .models import CustomUser
 
 
 class CustomUserView(APIView):
     def get(self, request):
-        queryset = CustomUser.objects.all()
-        serializer = CustomUserSerializer(queryset, many=True)
+        order_by = request.GET.get("order_by", "")
+        sort_order = request.GET.get("sort_order", "")
+
+        users = CustomUser.objects.filter(is_superuser=False, is_active=True)
+
+        if order_by:
+            sort_order = "-" if sort_order == "desc" else ""
+
+            users = users.order_by(sort_order + order_by)
+
+        serializer = MinifiedCustomUserSerializer(users, many=True)
 
         return success_response(serializer.data)
