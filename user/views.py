dbf66d465f9997e705cd414978a7ebe29c1fea95
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-10-12 13:31:44 -0300

    feat: create error handling in get_user_by_id

--- user/views.py
+++ user/views.py
@@ -31,8 +31,18 @@ class CustomUserView(viewsets.GenericViewSet):
 
     @action(detail=True, methods=["get"])
     def get_user_by_id(self, request, user_id):
-        user = CustomUser.objects.get(id=user_id)
+        try:
+            user = CustomUser.objects.get(id=user_id)
+            serializer = CustomUserSerializer(user)
 
-        serializer = CustomUserSerializer(user)
+            return success_response(serializer.data)
 
-        return success_response(serializer.data)
+        except CustomUser.DoesNotExist:
+            return error_response(
+                message="Prisoner not found.", status=status.HTTP_404_NOT_FOUND
+            )
+
+        except Exception as e:
+            return error_response(
+                message=str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
