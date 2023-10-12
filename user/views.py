bc0a5ed2e4b961a6d9472d9796f71dbcb24ebb49
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-10-12 03:03:50 -0300

    feat: create get_user_by_id

--- user/views.py
+++ user/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from help_or_gtfo_backend.utils import success_response, error_response
-from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import CustomUserSerializer
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from .serializers import CustomUserSerializer, MinifiedCustomUserSerializer
 from .models import CustomUser
 
 
-class CustomUserView(APIView):
-    def get(self, request):
+class CustomUserView(viewsets.GenericViewSet):
+    @action(detail=True, methods=["get"])
+    def get_users(self, request):
         order_by = request.GET.get("order_by", "")
         sort_order = request.GET.get("sort_order", "")
 
@@ -23,6 +25,14 @@ class CustomUserView(APIView):
 
             users = users.order_by(sort_order + order_by)
 
-        serializer = CustomUserSerializer(users, many=True)
+        serializer = MinifiedCustomUserSerializer(users, many=True)
+
+        return success_response(serializer.data)
+
+    @action(detail=True, methods=["get"])
+    def get_user_by_id(self, request, user_id):
+        user = CustomUser.objects.get(id=user_id)
+
+        serializer = CustomUserSerializer(user)
 
         return success_response(serializer.data)
