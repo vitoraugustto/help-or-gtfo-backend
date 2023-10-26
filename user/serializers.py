538ece5c42d42c3fa7c61a32e2f8ed7dc376a8be
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-10-25 23:46:39 -0300

    feat: remove useless CustomUserCompletedExpeditionsSerializer

--- user/serializers.py
+++ user/serializers.py
@@ -38,21 +38,6 @@ class CompletedExpeditionsSerializer(serializers.ModelSerializer):
         ]
 
 
-class CustomUserCompletedExpeditionsSerializer(serializers.ModelSerializer):
-    completed_expeditions = serializers.SerializerMethodField()
-
-    class Meta:
-        model = CustomUser
-        fields = ["completed_expeditions"]
-
-    def get_completed_expeditions(self, obj):
-        completed_expeditions = CompletedExpeditions.objects.filter(user=obj)
-
-        serializer = CompletedExpeditionsSerializer(completed_expeditions, many=True)
-
-        return serializer.data
-
-
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
