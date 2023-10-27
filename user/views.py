e8ca4ee4cb0f096be5a2b7a35e79081870df6c18
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-10-26 23:43:20 -0300

    feat: create StandardPagination

--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.decorators import action
-from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import status
 
@@ -15,8 +15,24 @@ from .serializers import (
 )
 
 
+class StandardPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return success_response(
+            {
+                "count": self.page.paginator.count,
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link(),
+                "results": data,
+            }
+        )
+
+
 class CustomUserView(viewsets.GenericViewSet):
-    pagination_class = LimitOffsetPagination
+    pagination_class = StandardPagination
 
     @action(detail=True, methods=["get"])
     def get_users(self, request):
