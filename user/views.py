9caa8f81e5817302e9395e9e880258d5a765a1ff
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-10-29 23:52:10 -0300

    feat: remove page_size_query_param from StandardPagination

--- user/views.py
+++ user/views.py
@@ -17,7 +17,6 @@ from .serializers import (
 
 class StandardPagination(PageNumberPagination):
     page_size = 10
-    page_size_query_param = "page_size"
     max_page_size = 100
 
     def get_paginated_response(self, data):
