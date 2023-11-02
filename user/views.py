c7734287363f8935c03deb0dd605cc159f59740f
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-11-01 23:39:20 -0300

    feat: remove max size from StandardPagination

--- user/views.py
+++ user/views.py
@@ -17,7 +17,6 @@ from .serializers import (
 
 class StandardPagination(PageNumberPagination):
     page_size = 10
-    max_page_size = 100
 
     def get_paginated_response(self, data):
         return success_response(
