2bbf5ecc25cecdfaf735cbd0c881331cfd22fd03
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-11-03 23:38:24 -0300

    feat: remove next and previous from StandardPagination

--- user/views.py
+++ user/views.py
@@ -23,8 +23,6 @@ class StandardPagination(PageNumberPagination):
         return success_response(
             {
                 "count": self.page.paginator.count,
-                "next": self.get_next_link(),
-                "previous": self.get_previous_link(),
                 "results": data,
             }
         )
