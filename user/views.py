fa28ca0e00bc64c8067c4e0affedc94654998c6d
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-11-07 23:47:45 -0300

    feat: add page_size_query_param and max_page_size in StandardPagination

--- user/views.py
+++ user/views.py
@@ -16,7 +16,9 @@ from .serializers import (
 
 
 class StandardPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 5
+    page_size_query_param = "page_size"
+    max_page_size = 5
 
     def get_paginated_response(self, data):
         return success_response(
