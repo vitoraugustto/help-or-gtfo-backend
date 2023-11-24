36c5eb73df47210f83b01517c171dacfeed7f7bc
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-11-23 23:52:40 -0300

    feat: change StandardPagination param to LimitOffsetPagination

--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.decorators import action
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import status
 
@@ -15,7 +15,7 @@ from .serializers import (
 )
 
 
-class StandardPagination(PageNumberPagination):
+class StandardPagination(LimitOffsetPagination):
     page_size = 5
     page_size_query_param = "page_size"
     max_page_size = 5
