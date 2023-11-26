e22843181657b0d5acfd292d5f5d78e1260a1b24
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-11-25 23:43:02 -0300

    style: remove unused import

--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.decorators import action
-from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import status
 
